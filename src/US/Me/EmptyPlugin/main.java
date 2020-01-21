561bd2d682c81697530a5004111b9ee52481c1f1
Tad Taylor <bittiez@gmail.com>
2020-01-21 05:54:58 -0700

    Initial commit

---
+++ src/US/Me/EmptyPlugin/main.java
@@ -0,0 +1,21 @@
+package US.Me.EmptyPlugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class main extends JavaPlugin implements Listener {
+    private static Logger log;
+
+    @Override
+    public void onEnable() {
+        log = getLogger();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    public boolean onCommand(CommandSender who, Command cmd, String label, String[] args) {
+        return false;
+    }
+}
