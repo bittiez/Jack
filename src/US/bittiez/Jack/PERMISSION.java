422d69b7853a53c827e2f7edac30e951a1ae8b56
bittiez <bittiez@gmail.com>
2020-01-22 09:25:59 -0700

    Added checks for permissions

---
+++ src/US/bittiez/Jack/PERMISSION.java
@@ -0,0 +1,6 @@
+package US.bittiez.Jack;
+
+public class PERMISSION {
+    public final static String RELOAD = "Jack.reload";
+    public final static String RESPOND = "Jack.respond";
+}
--- src/US/bittiez/Jack/main.java
+++ src/US/bittiez/Jack/main.java
@@ -32,45 +32,52 @@ public class main extends JavaPlugin implements Listener {
 
         mainConfig = new ConfigAccessor(this, "config.yml");
         chatConfig = new ConfigAccessor(this, "chat.yml");
-        if(!this.getDataFolder().exists()) this.getDataFolder().mkdirs();
+        if (!this.getDataFolder().exists()) this.getDataFolder().mkdirs();
         mainConfig.saveDefaultConfig();
         chatConfig.saveDefaultConfig();
         loadJack();
     }
 
-    private void loadJack(){
+    private void loadJack() {
         chatPrefix = mainConfig.getConfig().getString("require_prefix", "Hey Jack");
     }
 
     public boolean onCommand(CommandSender who, Command cmd, String label, String[] args) {
-        if(cmd.getName().equalsIgnoreCase("Jack")){
-            //Add /Jack reload
+        if (cmd.getName().equalsIgnoreCase("Jack") && who.hasPermission(PERMISSION.RELOAD)) {
+            if (args[0].equalsIgnoreCase("reload")) {
+                chatConfig.reloadConfig();
+                mainConfig.reloadConfig();
+                loadJack();
+                who.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Jack &7has been reloaded!"));
+            }
             return true;
         }
         return false;
     }
 
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent e){
-        if(!e.isCancelled() && !e.getMessage().isEmpty()) {
-            String message = e.getMessage();
-            if(!chatPrefix.isEmpty()){
-                //Prefix defined
-                if(message.toLowerCase().startsWith(chatPrefix.toLowerCase())){
-                    //Prefix found in this message
-                    Bukkit.getScheduler().runTaskAsynchronously(this, ()->handleMessage(e.getPlayer(), message));
+    public void onChat(AsyncPlayerChatEvent e) {
+        if (!e.isCancelled() && !e.getMessage().isEmpty()) {
+            if (e.getPlayer().hasPermission(PERMISSION.RESPOND)) {
+                String message = e.getMessage();
+                if (!chatPrefix.isEmpty()) {
+                    //Prefix defined
+                    if (message.toLowerCase().startsWith(chatPrefix.toLowerCase())) {
+                        //Prefix found in this message
+                        Bukkit.getScheduler().runTaskAsynchronously(this, () -> handleMessage(e.getPlayer(), message));
+                    }
+                } else {
+                    //No prefix defined
+                    Bukkit.getScheduler().runTaskAsynchronously(this, () -> handleMessage(e.getPlayer(), message));
                 }
-            } else {
-                //No prefix defined
-                Bukkit.getScheduler().runTaskAsynchronously(this, ()->handleMessage(e.getPlayer(), message));
             }
         }
     }
 
-    private void handleMessage(Player from, String message){
+    private void handleMessage(Player from, String message) {
         message = ChatColor.stripColor(message);
-        for (String listen : chatConfig.getConfig().getConfigurationSection("listen").getKeys(false)){
-            if(message.toLowerCase().contains(listen.toLowerCase())){
+        for (String listen : chatConfig.getConfig().getConfigurationSection("listen").getKeys(false)) {
+            if (message.toLowerCase().contains(listen.toLowerCase())) {
                 String response = mainConfig.getConfig().getString("response", "&3Jack: &7[RESPONSE]");
                 response = response.replace("[RESPONSE]", getResponse(chatConfig.getConfig().getString("listen." + listen)));
                 response = response.replace("[PLAYER]", from.getName());
@@ -81,9 +88,9 @@ public class main extends JavaPlugin implements Listener {
         }
     }
 
-    private String getResponse(String answerPath){
+    private String getResponse(String answerPath) {
         List<String> responses = chatConfig.getConfig().getStringList("answers." + answerPath);
-        if(responses.size() < 2)
+        if (responses.size() < 2)
             return responses.get(0);
         return responses.get(randomGenerator.nextInt(responses.size()));
     }
