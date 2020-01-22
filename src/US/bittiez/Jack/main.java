257d9d1ca92950010df162035abd8e2d90b7771d
bittiez <bittiez@gmail.com>
2020-01-22 06:59:20 -0700

    Many changes, plugin works as expected.

--- src/US/bittiez/Jack/main.java
+++ src/US/bittiez/Jack/main.java
@@ -15,6 +15,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.logging.Logger;
 
 public class main extends JavaPlugin implements Listener {
@@ -42,24 +43,24 @@ public class main extends JavaPlugin implements Listener {
     }
 
     public boolean onCommand(CommandSender who, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("Jack")){
+            //Add /Jack reload
+            return true;
+        }
         return false;
     }
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e){
-        log.info("Made it this far yo");
         if(!e.isCancelled() && !e.getMessage().isEmpty()) {
-            log.info("Message isnt cancelled or empty");
             String message = e.getMessage();
             if(!chatPrefix.isEmpty()){
-                log.info("chatPrefix is not empty");
                 //Prefix defined
                 if(message.toLowerCase().startsWith(chatPrefix.toLowerCase())){
                     //Prefix found in this message
                     Bukkit.getScheduler().runTaskAsynchronously(this, ()->handleMessage(e.getPlayer(), message));
                 }
             } else {
-                log.info("chatPrefix is empty");
                 //No prefix defined
                 Bukkit.getScheduler().runTaskAsynchronously(this, ()->handleMessage(e.getPlayer(), message));
             }
@@ -68,14 +69,14 @@ public class main extends JavaPlugin implements Listener {
 
     private void handleMessage(Player from, String message){
         message = ChatColor.stripColor(message);
-        for (String listen : chatConfig.getConfig().getStringList("listen")){
-            log.info("Try to match [" + message + "] against [" + listen + "]");
+        for (String listen : chatConfig.getConfig().getConfigurationSection("listen").getKeys(false)){
             if(message.toLowerCase().contains(listen.toLowerCase())){
                 String response = mainConfig.getConfig().getString("response", "&3Jack: &7[RESPONSE]");
-                 response = response.replace("[RESPONSE]", getResponse(chatConfig.getConfig().getString("listen." + listen)));
-                 response = response.replace("[PLAYER]", from.getName());
-                 response = ChatColor.translateAlternateColorCodes('&', response);
-                 from.sendMessage(response);
+                response = response.replace("[RESPONSE]", getResponse(chatConfig.getConfig().getString("listen." + listen)));
+                response = response.replace("[PLAYER]", from.getName());
+                response = ChatColor.translateAlternateColorCodes('&', response);
+                from.sendMessage(response);
+                break;
             }
         }
     }
